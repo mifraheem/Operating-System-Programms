fe5ddd60665e153ddfea075bd09c958b06bf7403
mifraheem <mifrahim017@gmail.com>
2023-08-29 19:45:33 +0100

    comments are added for 1_first program fork system call

--- 1_process_system_calls.c
+++ 1_process_system_calls.c
@@ -1,23 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
+#include<stdio.h> 
+#include<stdlib.h>  // for exit()
+#include<unistd.h>  // Unix Standard => fork(), execlp(), getpid(),
+#include<sys/wait.h> // wait.
 void main()
 
 {
-
 	int pid;
-	pid=fork();
-	if(pid<0) {
+	pid=fork(); // fork() => duplicate the current process, createing a parent-child relationship
+	
+	// fork will return:
+	// -ve value: if creating new process is failled.
+	// 0 : newly created child process.
+	// +ve value: return value to caller or parent. value contains process id of newsly created child process.
+
+	if(pid<0) {  // check if fork (creating new process) faills ( on faill it will return a -ve value)
 
 		printf("fork failed");
 		exit(1);
 	}
 
-	else if(pid==0)
-
+	else if(pid==0)  // newly created child process. if pid is 0 i means that child process is running.
 	{
-		execlp("whoami","ls",NULL);
+		// execlp("date","ls",NULL); // whoami will print the current username. ls(will not do anything) if whoami replace with ls it will print names of all files in directory
+		execlp("whoami","ls",NULL); // whoami will print the current username. ls(will not do anything) if whoami replace with ls it will print names of all files in directory
+        // execlp command is used to run another command inside program. full form -> execute list with path
 		exit(0);
 	}
 
@@ -25,7 +31,8 @@ void main()
 
 	{
 
-		printf("\n Process id is -%d\n",getpid());
+		printf("\n Process id is: %d\n",getpid());
+        //getpid() is used to get current process id.
 		wait(NULL);
 		exit(0);
 
