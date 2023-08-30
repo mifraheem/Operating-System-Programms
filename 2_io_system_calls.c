528bc76cf61427d6c66b9ef971c04ac3183e90c6
M Ifraheem <mifrahim017@gmail.com>
2023-08-31 00:54:36 +0500

    comments added for 2_second program

--- 2_io_system_calls.c
+++ 2_io_system_calls.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h>  // Unix standard. used in program to use unix c methods.
 #include <string.h>
-#include <fcntl.h> 
+#include <fcntl.h>  // linux file handling.
 int main( )
 {
     int fd[2];
-    char buf1[25] = "just a test\n";
+    char buf1[50] = "Write this in first opened file.\n";
     char buf2[50];
-    fd[0] = open("file1", O_RDWR);
-    fd[1] = open("file2", O_RDWR);
-    write(fd[0], buf1, strlen(buf1));
-    printf("\n Enter the text now….");
-    scanf("\n %s", buf1);
+    fd[0] = open("file1.txt", O_RDWR); // O_RDWR is file flag that indicate that : open file in both read and write method.
+    fd[1] = open("file2.txt", O_RDWR);
+    write(fd[0], buf1, strlen(buf1)); //  this will write the string from buf1 array to file1.txt
+    printf("\n Enter the text now: "); 
+    scanf("%s", buf1); // must enter a signle string not in two words. 
     printf("\n Cat file1 is	\n hai");
-    write(fd[0], buf1, strlen(buf1));
-    lseek(fd[0], SEEK_SET, 0);
-    read(fd[0], buf2, sizeof(buf1));
-    write(fd[1], buf2, sizeof(buf2));
+    write(fd[0], buf1, strlen(buf1));  // now it will write the userinput in file1
+    lseek(fd[0], SEEK_SET, 0);        // Isseek => it will move the pointer in the begining of file after writing.(so that it can read data from beginning)
+    read(fd[0], buf2, sizeof(buf1)); // it will read file1 and the content of file1 will be stored in bug2 variable.
+    write(fd[1], buf2, sizeof(buf2)); // now the buf2 will write the stored data in file2.
     close(fd[0]);
     close(fd[1]);
 
